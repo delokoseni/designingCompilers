80e478e12be64762dd8421d51494590570720588
delokoseni <artur.b3lsh@gmail.com>
2026-04-06 12:49:32 +0700

    Добавил класс TriadGenerator, написал заготовку в файле TriadGenerator.h.

---
+++ TriadGenerator.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "SemanticTree.h"
+#include "TriadHelper.h"
+#include "GlobalData.h"
+#include <stack>
+
+class TriadGenerator
+{
+private:
+    SemanticTree* tree;
+    GlobalData* global;
+    int number = 0;
+    std::stack<Triad*> triads;
+
+    void deltaMatch();
+    void generateTriad(const type_lex operation);
+
+public:
+    TriadGenerator();
+    void setTree(SemanticTree*);
+    void setGlobal(GlobalData*);
+
+    // Арифметические и сравнения
+    void deltaAdd();
+    void deltaMinus();
+    void deltaDiv();
+    void deltaMult();
+    void deltaMod();
+    void deltaLess();
+    void deltaLessEq();
+    void deltaMore();
+    void deltaMoreEq();
+    void deltaShiftLeft();
+    void deltaShiftRight();
+    void deltaEval();
+    void deltaUnEval();
+
+    // Присваивание и операнды
+    void deltaAssign();
+    void deltaPushOperand(bool);
+    void deltaPushOperator(const type_lex op);
+
+    // Управление while
+    void deltaWhileStart();
+    void deltaWhileCondition();
+    void deltaWhileEnd();
+
+    // Вызов функции без параметров
+    void deltaCallFunction();
+
+    // Печать триад
+    void printTriad();
+};
